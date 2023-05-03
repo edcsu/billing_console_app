7c1b4ab585d79ec0d94c9c27dbfd84189f5d5c46
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-05-04 02:56:35 +0300

    refactor: create get input function

--- main.go
+++ main.go
@@ -7,12 +7,18 @@ import (
 	"strings"
 )
 
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Type in the new bill name: ")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := getInput("Type in the new bill name: ", reader)
 
 	b := newBill(name)
 	fmt.Println("Created the bill - ", b.name)
